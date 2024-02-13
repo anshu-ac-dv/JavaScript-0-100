6cc7a2cb36a8d3517c903083fd6b6f520b2566c3
Anshu Kumar <anshu.ac.dv@gmail.com>
2024-02-13 20:02:27 +0530

    Initial Commit

---
+++ Variables.js
@@ -0,0 +1,20 @@
+var a = 5;
+var b = 5;
+var c = "Anshu";
+console.log(a + b + c);
+console.log(typeof a, typeof b, typeof c);
+
+
+// var 55a;  This is invalid type of declear a variables. This is for declear globley variables for declearation....
+
+// The uses of const variables for constant for not allow to changes in this variables.
+
+// Always use let for declear any variables. let not for global variabes.
+
+
+let d = 55;
+console.log(a);
+
+
+// Always use let for declear any variables.
+
--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-// alert("Hello my name is Anshu Kumar")
+alert("Hello my name is Anshu Kumar")
 
 
-// console.log("Hello this is console")
+console.log("Hello this is console")
 
 
-// var a = prompt("Enter any number")
-// console.log("Your number is " +a)
+var a = prompt("Enter any number")
+console.log("Your number is " +a)
 
 
-// var isTrue = confirm("If you want to exit form this website click ok otherwaise click cancle")
-// if (isTrue) {
-//     console.log("you have scessfully exit form this website")
-// }
-// else{
-//     console.log("you have not scessfully exit form this website")
-// }
-
-
+var isTrue = confirm("If you want to exit form this website click ok otherwaise click cancle")
+if (isTrue) {
+    console.log("you have scessfully exit form this website")
+}
+else{
+    console.log("you have not scessfully exit form this website")
+}
