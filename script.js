5098502608f6e4b52c8d8f1b5d8a8bfcd31acff2
Anshu Kumar <anshu.ac.dv@gmail.com>
2024-02-13 18:21:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("Hello my name is Anshu Kumar")
+
+
+// console.log("Hello this is console")
+
+
+// var a = prompt("Enter any number")
+// console.log("Your number is " +a)
+
+
+// var isTrue = confirm("If you want to exit form this website click ok otherwaise click cancle")
+// if (isTrue) {
+//     console.log("you have scessfully exit form this website")
+// }
+// else{
+//     console.log("you have not scessfully exit form this website")
+// }
+
+
