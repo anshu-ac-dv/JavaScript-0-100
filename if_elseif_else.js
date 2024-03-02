c6b5372a25be4b34342649114c2f13de86960435
Anshu Kumar <anshu.ac.dv@gmail.com>
2024-03-02 18:19:53 +0530

    Initial commit

--- if_elseif_else.js
+++ if_elseif_else.js
@@ -3,9 +3,9 @@
 let age = 18;
 if (age > 18) {
     console.log("You cannot drive");
-} else if (age == 18){
+} else if (age == 18) {
     console.log("You can drive");
 }
-else{
+else {
     console.log("You cannot drive");
 }
